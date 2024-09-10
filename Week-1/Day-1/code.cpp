8c3ff7e41748860ea354ca91541c12a1e900b191
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-09-10 14:57:08 +0530

    initial commit

---
+++ Week-1/Day-1/code.cpp
@@ -0,0 +1,39 @@
+// #include <iostream>
+// using namespace std;
+
+// // This is the starting point of the execution
+// int main(){
+
+//    return 0;
+// }
+
+// Boilerplate code - comment
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Kajal\n" << "Pandey\n";
+//     return 0;
+// }
+
+
+// Variables => containers to store data
+// Data Types =>
+// 1. int - 4 Bytes
+// 2. char
+// 3. float
+// 4. bool
+// 5. double
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 25;
+    // cout << sizeof(age) << endl;
+    cout << age << endl;
+    return 0;
+}

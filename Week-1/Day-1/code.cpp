0216996b269fd17997e2af46b69c40cd3cbfe755
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-09-11 21:49:15 +0530

    Day-1 complete

--- Week-1/Day-1/code.cpp
+++ Week-1/Day-1/code.cpp
@@ -1,31 +1,32 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// // This is the starting point of the execution
-// int main(){
+// This is the starting point of the execution
+int main(){
 
-//    return 0;
-// }
+   return 0;
+}
 
 // Boilerplate code - comment
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main()
-// {
-//     cout << "Kajal\n" << "Pandey\n";
-//     return 0;
-// }
+int main()
+{
+    cout << "Kajal\n" << "Pandey\n";
+    return 0;
+}
 
 
 // Variables => containers to store data
 // Data Types =>
+// -> Primitive Data Type
 // 1. int - 4 Bytes
-// 2. char
-// 3. float
-// 4. bool
-// 5. double
+// 2. char - 1 Byte
+// 3. float - 4 Byte
+// 4. bool - 1 Byte
+// 5. double - 8 Byte
 
 #include <iostream>
 using namespace std;
@@ -33,7 +34,38 @@ using namespace std;
 int main()
 {
     int age = 25;
+    char grade = 'A';
+    float PI = 3.14f;
+    bool isSafe = true; // true -> 1 & false -> 0
+    double price = 100.99;
+
     // cout << sizeof(age) << endl;
+    // cout << sizeof(grade) << endl;
+    // cout << sizeof(PI) << endl;
+    // cout << sizeof(isSafe) << endl;
+    // cout << sizeof(price) << endl;
     cout << age << endl;
+    cout << grade << endl;
+    cout << PI << endl;
+    cout << isSafe << endl;
+    cout << price << endl;
+    return 0;
+}
+
+
+// Type Casting
+// Convert data from one type to another
+#include <iostream>
+using namespace std;
+
+int main(){
+    char grade = 'A'; //65
+    double price = 100.99;
+
+
+    int value = grade;
+    int newPrice = (int)price;
+    cout << value << endl;
+    cout << newPrice << endl;
     return 0;
 }
---
+++ Week-1/Day-1/code2.cpp
@@ -0,0 +1,34 @@
+// Input in C++
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int age;
+//     double price;
+//     // cout << "Enter the age : ";
+//     cout << "Enter the price : ";
+//     // cin >> age;
+//     cin >> price;
+
+//     // cout << "Your age is : " << age << endl;
+//     cout << "You entered price = " << price << endl;
+//     return 0;
+// }
+
+// Operators
+// -> Arithmetic = +, -, *, /, %
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 5;
+
+    cout << "sum = " << (a + b) << endl;        // 15
+    cout << "difference = " << (a - b) << endl; // 5
+    cout << "product = " << (a * b) << endl;    // 50
+    cout << "division = " << (a / b) << endl;   // 2
+    cout << "modulo = " << (a % b) << endl;     // 0
+    return 0;
+}

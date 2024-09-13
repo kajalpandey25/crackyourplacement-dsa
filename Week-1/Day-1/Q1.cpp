6494ada8f0807004e2cd45c0c8d609a2877f42cd
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-09-13 17:34:05 +0530

    Day-1 complete

---
+++ Week-1/Day-1/Q1.cpp
@@ -0,0 +1,20 @@
+// Sum of 2 numbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "enter a : ";
+    cin >> a;
+
+    cout << "enter b : ";
+    cin >> b;
+
+    int sum = a+b;
+    cout << "sum = " << sum << endl;
+    return 0;
+}
+
+
--- Week-1/Day-1/code2.cpp
+++ Week-1/Day-1/code2.cpp
@@ -1,19 +1,19 @@
 // Input in C++
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int age;
-//     double price;
-//     // cout << "Enter the age : ";
-//     cout << "Enter the price : ";
-//     // cin >> age;
-//     cin >> price;
+int main(){
+    int age;
+    double price;
+    // cout << "Enter the age : ";
+    cout << "Enter the price : ";
+    // cin >> age;
+    cin >> price;
 
-//     // cout << "Your age is : " << age << endl;
-//     cout << "You entered price = " << price << endl;
-//     return 0;
-// }
+    // cout << "Your age is : " << age << endl;
+    cout << "You entered price = " << price << endl;
+    return 0;
+}
 
 // Operators
 // -> Arithmetic = +, -, *, /, %
@@ -30,5 +30,69 @@ int main()
     cout << "product = " << (a * b) << endl;    // 50
     cout << "division = " << (a / b) << endl;   // 2
     cout << "modulo = " << (a % b) << endl;     // 0
+    return 0;
+}
+
+// -> Relational = <, <=, >, >=, ==, !=
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << (3 < 5) << endl;  // true -> 1
+    cout << (3 > 5) << endl;  // false -> 0
+    cout << (3 <= 3) << endl; // true -> 1
+    cout << (3 >= 5) << endl; // false -> 0
+    cout << (3 == 3) << endl; // true -> 1
+    cout << (3 != 5) << endl; // true -> 1
+    return 0;
+}
+
+// -> Logical = OR(||), AND(&&), NOT(!)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << !(3 > 1) << endl; // false -> 0
+    cout << ((3 < 1) || (3 < 5)) << endl; // true -> 1
+    cout << ((3 > 1) && (3 > 4)) << endl; // false -> 0
+    return 0;
+}
+
+
+// Unary Operators
+// -> Increment++
+// -> Decrement--
+
+
+// -> Increment++ (Post-increment)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    int b = a++;  // kaam : update
+    cout << "b = " << b << endl; // 10
+    cout << "a = " << a << endl; // 11
+    return 0;
+}
+
+
+// -> ++Increment (Pre-increment)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    int b = ++a;  // kaam : update
+    cout << "b = " << b << endl; // 11
+    cout << "a = " << a << endl; // 11
     return 0;
 }

09c10c0c04473754e9d35f8dbe21bf697bece0b9
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-09-28 12:47:15 +0530

    Day-3 complete

--- Week-1/Day-3/practice3.cpp
+++ Week-1/Day-3/practice3.cpp
@@ -4,27 +4,27 @@
 //    3 3
 //      4
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n = 4;
+int main(){
+    int n = 4;
 
-//     int num = 1;
-//     for (int i = 0; i < n; i++){
-//         // spaces
-//         for (int j = 0; j < i; j++){
-//             cout << " ";
-//         }
+    int num = 1;
+    for (int i = 0; i < n; i++){
+        // spaces
+        for (int j = 0; j < i; j++){
+            cout << " ";
+        }
 
-//         // nums
-//         for (int j = 0; j < n - i; j++){
-//             cout << (i + 1);
-//         }
-//         cout << endl;
-//     }
-//     return 0;
-// }
+        // nums
+        for (int j = 0; j < n - i; j++){
+            cout << (i + 1);
+        }
+        cout << endl;
+    }
+    return 0;
+}
 
 // Q.2 -> Inverted Triangle Pattern(char)
 // A A A A
@@ -32,30 +32,28 @@
 //     C C
 //       D
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     int n = 4;
+int main(){
+    int n = 4;
 
-//     char ch = 'A';
-//     for (int i = 0; i < n; i++){
-//         // spaces
-//         for (int j = 0; j < i; j++){
-//             cout << " ";
-//         }
+    char ch = 'A';
+    for (int i = 0; i < n; i++){
+        // spaces
+        for (int j = 0; j < i; j++){
+            cout << " ";
+        }
 
-//         // nums
-//         for (int j = 0; j < n - i; j++){
-//             cout << ch;
-//         }
-//         cout << endl;
-//         ch++;
-//     }
-//     return 0;
-// }
-
-
+        // nums
+        for (int j = 0; j < n - i; j++){
+            cout << ch;
+        }
+        cout << endl;
+        ch++;
+    }
+    return 0;
+}
 
 // Q.3 -> Pyramid Pattern
 //    1
@@ -89,5 +87,116 @@ int main(){
     return 0;
 }
 
-
 // Q.4 -> Hollow Diamond Pattern
+//    *
+//   * *
+//  *   *
+// *     *
+//  *   *
+//   * *
+//    *
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+
+    // top
+    for (int i = 0; i < n; i++)
+    {
+        // spaces
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+
+        if (i != 0)
+        {
+            // spaces
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // bottom
+     for(int i=0; i<n-1; i++){ // 0 to n-2
+        //spaces
+      for(int j=0; j<i+1; j++){
+        cout << " ";
+      }
+        cout << "*";
+        if(i != n-2){
+            //spaces
+         for(int j=0; j<2*(n-i)-5; j++){
+            cout << " ";
+         }
+            cout << "*";
+        }
+        cout << endl;
+     }
+
+    return 0;
+}
+
+
+
+// Q.5 -> Butterfly Pattern
+// *      *
+// **    **
+// ***  ***
+// ********
+// ********
+// ***  ***
+// **    **
+// *      *
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=4;
+
+
+    // Upper part of the butterfly
+    for (int i = 1; i <= n; i++) {
+        // Left half stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        // Spaces in the middle
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+        // Right half stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Lower part of the butterfly
+    for (int i = n; i >= 1; i--) {
+        // Left half stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        // Spaces in the middle
+        for (int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+        // Right half stars
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

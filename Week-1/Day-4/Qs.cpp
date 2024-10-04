100676974d7624c21acfa66009e233ad20100158
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-10-04 17:04:33 +0530

    Day-4 complete

--- Week-1/Day-4/Qs.cpp
+++ Week-1/Day-4/Qs.cpp
@@ -60,3 +60,33 @@ int main() {
     cout << "sum = " << sumOfDigits(2356) << endl;
     return 0;
 }
+
+
+// Q.4 -> Calculate nCr binomial coefficient for n & r.
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact = 1;
+
+    for(int i=1; i<=n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+int nCr(int n , int r){
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_nmr = factorial(n-r);
+
+    return fact_n/ (fact_r * fact_nmr);
+}
+
+int main(){
+  int n =6, r=3;
+  cout << nCr(n,r) << endl;
+
+    return 0;
+}
---
+++ Week-1/Day-4/Qs2.cpp
@@ -0,0 +1,4 @@
+// Homework Question
+// Q.1 -> WAF to check if a number is prime or not.
+// Q.2 -> WAF to print all prime numbers from 1 to N.
+// Q.3 -> WAF to print nth Fibonacci.

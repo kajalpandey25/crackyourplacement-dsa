1908d9f36702a3901f017fa5245a569f2581528f
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2024-10-31 21:07:18 +0530

    Day-6 completed

---
+++ Week-1/Day-6/bitwise-operators.cpp
@@ -0,0 +1,37 @@
+// Bitwise Operators
+// 1. Bitwise & (AND)
+// 2. Bitwise | (OR)
+// 3. Bitwise ^ (XOR)
+// 4. Bitwise << (left shift operator)
+// 5. Bitwise >> (right shift operator)
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int a=4, b=8;
+
+//     cout << (a & b) << endl;
+//    cout << (a | b) << endl;
+//   cout << (a ^ b) << endl;
+    
+//     return 0;
+// }
+
+// Example-2
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // cout << (4 << 1) << endl;
+     cout << (8 >> 2) << endl;
+    return 0;
+}
+
+
+
+
+
+
